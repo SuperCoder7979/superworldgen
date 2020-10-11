403b69bd27d27f2e89fec53423cdcd802fe8edae
SuperCoder7979 <k.pranav@gmail.com>
2020-10-11 16:14:25 -0400

    initialize repo

---
+++ src/main/java/supercoder79/superworldgen/SuperWorldGen.java
@@ -0,0 +1,11 @@
+package supercoder79.superworldgen;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class SuperWorldGen implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
